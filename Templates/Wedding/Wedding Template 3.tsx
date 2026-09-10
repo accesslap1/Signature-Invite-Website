cf01b39234a90664d3d2998b85f486e95597738e
accesslap1 <accesslap1@gmail.com>
2026-09-10 13:13:44 +0200

    Fix Wedding Template 3 hero image

--- Templates/Wedding/Wedding Template 3.tsx
+++ Templates/Wedding/Wedding Template 3.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useRef, useState, type CSSProperties, type ReactNode } from "react";
-const img1 = "https://raw.githubusercontent.com/accesslap1/Signature-Invite-Website/main/Media/Wedding%20Template%203/hero.png";
+const img1 = "https://raw.githubusercontent.com/accesslap1/Signature-Invite-Website/main/Media/Wedding%20Template%203/hero.webp";
 import img2 from "@/imports/Frame52/9ba69ff2867fc8eb7ee1662dee6dad98cd05b5a5.png";
 import img3 from "@/imports/Frame52/9e31a0e9ae441eafb41f0326c9e4d915838d2e34.png";
 import img4 from "@/imports/Frame52/c1e75502a39e9deaa98d73dd483de7ba4cdec0ec.png";
